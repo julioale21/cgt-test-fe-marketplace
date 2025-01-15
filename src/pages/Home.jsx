5527c574ab80e4a091690e2ffd1dbe5cf2a898f7
julioale21 <30907777+julioale21@users.noreply.github.com>
2025-01-15 00:20:37 -0300

    feat: added basic home design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import { Container, Typography } from '@mui/material';
+import { products } from '../constants/products';
+import { ProductList } from '../components';
 
 const Home = () => {
   return (
-    <div>
-      Welcome to our shop!
-      <p>
-        You are probably interested in <Link to="/products/a">A</Link>.
-      </p>
-      <p>
-        Check out the newest product <Link to="/products/b">B</Link>!
-      </p>
-    </div>
+    <Container maxWidth="lg" sx={{ py: 8 }}>
+      <Typography variant="h3" component="h1" sx={{ mt: 4, mb: 4 }}>
+        3D Marketplace
+      </Typography>
+
+      <ProductList products={products} />
+    </Container>
   );
 };
 
