6689041e429f7202414bdaa71575b6b23108e9d6
julioale21 <30907777+julioale21@users.noreply.github.com>
2025-01-15 00:20:21 -0300

    feat: added product list basic validation and proptypes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Grid } from '@mui/material';
 import { ProductCard } from './ProductCard';
 
 const ProductList = ({ products }) => {
+  if (!products?.length) {
+    return null;
+  }
+
   return (
     <Grid container spacing={4}>
       {products.map((product) => (
@@ -10,6 +15,23 @@ const ProductList = ({ products }) => {
       ))}
     </Grid>
   );
+};
+
+ProductList.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+      format: PropTypes.arrayOf(PropTypes.string).isRequired,
+      polygons: PropTypes.string.isRequired,
+      textures: PropTypes.string.isRequired
+    })
+  ).isRequired
 };
 
 export { ProductList };
