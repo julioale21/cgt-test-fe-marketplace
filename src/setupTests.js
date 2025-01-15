1784ff979d5e1db9c656cc2b1c400275a1892ccb
julioale21 <30907777+julioale21@users.noreply.github.com>
2025-01-15 00:37:56 -0300

    chore(config tests): added basic scripts and test config

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock para react-router-dom
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  BrowserRouter: ({ children }) => <div>{children}</div>,
+  Link: ({ children, to }) => <a href={to}>{children}</a>
+}));
