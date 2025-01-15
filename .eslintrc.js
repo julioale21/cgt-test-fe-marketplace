b2832155ae525ad1db1467bee47299c5da1b6943
julioale21 <30907777+julioale21@users.noreply.github.com>
2025-01-15 00:05:43 -0300

    fix(format):fixed eslint and prettier format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,35 @@
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'off',
+    'comma-dangle': ['error', 'never'], 
     'no-unused-vars': 'warn',
-    'no-console': 'off',
+    'no-console': 'off'
   },
   settings: {
     react: {
-      version: 'detect',
-    },
-  },
+      version: 'detect'
+    }
+  }
 };
