36aa3e1bdc2231d9c9568fe72c945cd76bd8660e
julioale21 <30907777+julioale21@users.noreply.github.com>
2025-01-15 23:16:17 -0300

    chore: updated header padding

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,32 +1,33 @@
-// src/components/shared/Header/Header.jsx
 import { Link } from 'react-router-dom';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { AppBar, Toolbar, Typography, Badge } from '@mui/material';
+import { AppBar, Toolbar, Typography, Badge, Container } from '@mui/material';
 import PropTypes from 'prop-types';
 
 const Header = ({ cartItemsCount = 0 }) => {
   return (
     <AppBar position="static">
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography
-          variant="h6"
-          component={Link}
-          to="/"
-          sx={{ textDecoration: 'none', color: 'inherit' }}
-        >
-          Marketplace
-        </Typography>
-        <div>
-          <Link to="/" style={{ textDecoration: 'none', color: 'inherit', marginRight: '20px' }}>
-            Home
-          </Link>
-          <Link to="/cart" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <Badge badgeContent={cartItemsCount} color="error" max={99}>
-              <ShoppingCartIcon />
-            </Badge>
-          </Link>
-        </div>
-      </Toolbar>
+      <Container maxWidth="lg" disableGutters>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', px: { xs: 2, sm: 3 } }}>
+          <Typography
+            variant="h6"
+            component={Link}
+            to="/"
+            sx={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            Marketplace
+          </Typography>
+          <div>
+            <Link to="/" style={{ textDecoration: 'none', color: 'inherit', marginRight: '20px' }}>
+              Home
+            </Link>
+            <Link to="/cart" style={{ textDecoration: 'none', color: 'inherit' }}>
+              <Badge badgeContent={cartItemsCount} color="error" max={99}>
+                <ShoppingCartIcon />
+              </Badge>
+            </Link>
+          </div>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
